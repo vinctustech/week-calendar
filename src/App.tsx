87ebf7065b19acf1cd03d1560f65712df1a1c7f9
Edward A Maxedon <edadma@gmail.com>
2025-03-17 15:46:24 -0400

    SC-1172 test code

--- src/App.tsx
+++ src/App.tsx
@@ -6,48 +6,6 @@ import { CalendarEvent, WeekCalendar } from 'src/WeekCalendar'
 
 const App: FC = () => {
   const [value, setValue] = useState<Dayjs>(dayjs())
-  const events: CalendarEvent[] = [
-    {
-      id: '1',
-      title: 'Morning Team Sync',
-      day: 1, // Monday
-      startTime: '09:05',
-      endTime: '10:00',
-      color: 'blue',
-    },
-    {
-      id: '2',
-      title: 'Client Call',
-      day: 1, // Monday
-      startTime: '10:35',
-      endTime: '11:00',
-      color: 'green',
-    },
-    {
-      id: '3',
-      title: 'Product Discussion',
-      day: 2, // Tuesday
-      startTime: '09:15',
-      endTime: '10:30',
-      color: 'orange',
-    },
-    {
-      id: '4',
-      title: 'Weekly Planning',
-      day: 3, // Wednesday
-      startTime: '13:00',
-      endTime: '14:30',
-      color: 'blue',
-    },
-    {
-      id: '5',
-      title: 'Lunch with Team',
-      day: 5, // Friday
-      startTime: '12:00',
-      endTime: '13:00',
-      color: 'green',
-    },
-  ]
 
   return (
     <Card>
@@ -58,5 +16,48 @@ const App: FC = () => {
     </Card>
   )
 }
+
+const events: CalendarEvent[] = [
+  {
+    id: '1',
+    title: 'Morning Team Sync',
+    day: 1, // Monday
+    startTime: '09:05',
+    endTime: '10:00',
+    color: 'blue',
+  },
+  {
+    id: '2',
+    title: 'Client Call',
+    day: 1, // Monday
+    startTime: '10:35',
+    endTime: '11:00',
+    color: 'green',
+  },
+  {
+    id: '3',
+    title: 'Product Discussion',
+    day: 2, // Tuesday
+    startTime: '09:15',
+    endTime: '10:30',
+    color: 'orange',
+  },
+  {
+    id: '4',
+    title: 'Weekly Planning',
+    day: 3, // Wednesday
+    startTime: '13:00',
+    endTime: '14:30',
+    color: 'blue',
+  },
+  {
+    id: '5',
+    title: 'Lunch with Team',
+    day: 5, // Friday
+    startTime: '12:00',
+    endTime: '13:00',
+    color: 'green',
+  },
+]
 
 export default App
